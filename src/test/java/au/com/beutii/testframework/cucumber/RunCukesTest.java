52f35064249fa3ae3c3e13912009c7f1505341c4
Nick Liang <nliang@hcf.com.au>
2018-07-30 16:05:11 +1000

    zip packaging

--- src/test/java/au/com/beutii/testframework/cucumber/RunCukesTest.java
+++ src/test/java/au/com/beutii/testframework/cucumber/RunCukesTest.java
@@ -18,44 +18,7 @@ import cucumber.api.junit.Cucumber;
 			    }, 
 		monochrome = true,
 		tags = { 
-//				"~@ignore","~@NotToBeRunOnProd"
-				"~@ignore",			
-				
-//				"@EAPP"				
-//				"@DPcustomiseCoverP2P , @DPshowMePackagesP2P , @DPambulanceCoverP2P,@MemberLogin,@MemberForgotPassword, @CostCalculator, @Redirection, @PetInsurance,@MicrositeJoin,@ContactusForm,@TravelHCFmember,@TravelNotHCFmember,@Life,@MemberForgotPasswordNegetive,@MemberLoginOverseasMsg, @EAPP"
-//				"@DPcustomiseCoverP2P , @DPshowMePackagesP2P , @DPambulanceCoverP2P,@CostCalculator, @Redirection, @JoinOnline,@JoinOnlineFamilyCover,@PetInsurance,@MicrositeJoin,@ContactusForm,@TravelHCFmember,@TravelNotHCFmember,@Life,@MemberLoginOverseasMsg"
-//				"@JoinOnline"
-//				"StudentDepRegistration"				
-//				"@DPcustomiseCoverP2P , @DPshowMePackagesP2P , @DPambulanceCoverP2P,@MemberLogin,@MemberForgotPassword, @CostCalculator, @Redirection, @JoinOnline,@JoinOnlineFamilyCover,@PetInsurance,@MicrositeJoin,@ContactusForm,@TravelHCFmember,@TravelNotHCFmember,@Life,@MemberForgotPasswordNegetive,@MemberLoginOverseasMsg"
-//				"@PetInsurance"
-//				"@CallWaiting"
-//				"@ContactusForm"
-//				"@TravelHCFmember"
-//				"@MemberLogin"
-//				"@Life"
-//				"@Redirection, @TravelHCFmember"
-//				"@MemberForgotPassword",
-//				"@DPcustomiseCoverP2P , @DPshowMePackagesP2P , @DPambulanceCoverP2P, @Redirection, @MemberForgotPassword, @JoinOnline",	
-//				"@DPcustomiseCoverP2P",
-//				"@DPshowMePackagesP2P",
-//				"@DPambulanceCoverP2P",
-//				"@CustomiseCover",
-//				"@JoinOnline",
-//				"@Notificationpage",
-//				"@LocateALocalBranch",
-//				"@PathToPurchase",
-//				"@ReCalculation",
-//				"@HomePage",
-				"@Amazon",
-//				"@PathToPurchaseREST",
-//				"@SwitchingFunds"
-//				"@StaticMediaRelease",
-//				"@SwitchingFunds",
-//				"@CustomiseCover"
-//				"@EmailQuote",
-//				"@MemberClaim",
-//				"@Travel",
-//				"@HomePage,@LocateALocalBranch,@StaticMediaRelease",			
+				"@Amazon"
 				})
 public class RunCukesTest {
 	public RunCukesTest(){
